cbf2404b9b2b52ee9b8ca140300ce1037e40b9f8
Viroopaksh Chekuri <69083163+VIROOPAKSHC@users.noreply.github.com>
2026-06-24 07:32:12 +0000

    Completed lab1

--- CSE224/lab1-main/src/sort.go
+++ CSE224/lab1-main/src/sort.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"sort"
 )
+
+type Record struct {
+	length []byte
+	key    []byte
+	value  []byte
+}
+
+type Cmp []Record
+
+func (a Cmp) Len() int      { return len(a) }
+func (a Cmp) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a Cmp) Less(i, j int) bool {
+	return bytes.Compare(a[i].key, a[j].key) < 0
+}
 
 func ReadBigEndianUint32(buffer []byte) uint32 {
 	if len(buffer) < 4 {
@@ -44,6 +62,62 @@ func main() {
 	if len(os.Args) != 3 {
 		log.Fatalf("Usage: %v inputfile outputfile\n", os.Args[0])
 	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error opening the file.")
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var records []Record
+	for {
+		var record Record
+		length := make([]byte, 4)
+		err = binary.Read(reader, binary.BigEndian, &length)
 
-	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Failed to read 4 bytes for length :", err)
+		}
+		n := ReadBigEndianUint32(length)
+		record.length = length
+		fmt.Println("Read length :", length)
+
+		key := make([]byte, 10)
+		_, err := io.ReadFull(reader, key)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Failed to read key :", err)
+		}
+		record.key = key
+
+		value := make([]byte, n-10)
+		_, err = io.ReadFull(reader, value)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Failed to read value :", err)
+		}
+		record.value = value
+
+		records = append(records, record)
+	}
+
+	output, err := os.Create(os.Args[2])
+	if err != nil {
+		fmt.Println("Failed creating output file :", output)
+	}
+	defer output.Close()
+
+	sort.Sort(Cmp(records))
+	for i := 0; i < len(records); i++ {
+		record := records[i]
+		buf := make([]byte, 0, ReadBigEndianUint32(record.length)+4)
+		buf = append(buf, record.length...)
+		buf = append(buf, record.key...)
+		buf = append(buf, record.value...)
+		output.Write(buf)
+	}
 }
